0032c136e71896a799c8b2e3a2d218707f6168e0
albertgriso <albert.griso.mendez@gmail.com>
2021-01-28 17:05:28 +0100

    Init npm, express and server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const port = 3000;
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("src", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log("Server running...");
+    console.log(`Listening at http://localhost:${port}`);
+});
