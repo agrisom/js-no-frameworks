c485943387f29885cea249fe6f0a341a0d6a6c12
Albert Griso <albert.griso.mendez@gmail.com>
2021-01-29 20:41:37 +0100

    Ready to run on heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
